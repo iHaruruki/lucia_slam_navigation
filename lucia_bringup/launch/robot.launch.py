651bdb239f12cd8825c343c72e13807ceec27b41
Haruki Isono <haruki.isono861@gmail.com>
2024-11-01 17:56:26 +0900

    add: lucia_bringup

---
+++ lucia_bringup/launch/robot.launch.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+#
+# Copyright 2019 ROBOTIS CO., LTD.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Authors: Darby Lim
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch.substitutions import ThisLaunchFileDir
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    #TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+    #LDS_MODEL = os.environ['LDS_MODEL']
+    #LDS_LAUNCH_FILE = '/hlds_laser.launch.py'
+
+    #usb_port = LaunchConfiguration('usb_port', default='/dev/ttyACM0')
+
+    lucia_param_dir = LaunchConfiguration(
+        'lucia_param_dir',
+        default=os.path.join(
+            get_package_share_directory('lucia_bringup'),
+            'param',
+            'lucia.yaml'))
+
+    # LiDAR launch
+    lidar_pkg_dir = LaunchConfiguration(
+        'lidar_pkg_dir',
+        default=os.path.join(get_package_share_directory('urg_node2'), 'launch'))
+    LDS_LAUNCH_FILE = '/urg_node2.launch.py'
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
+
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value=use_sim_time,
+            description='Use simulation (Gazebo) clock if true'),
+
+        #DeclareLaunchArgument(
+        #    'usb_port',
+        #    default_value=usb_port,
+        #    description='Connected USB port with OpenCR'),
+
+        DeclareLaunchArgument(
+            'lucia_param_dir',
+            default_value=lucia_param_dir,
+            description='Full path to lucia parameter file to load'),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                [ThisLaunchFileDir(), '/lucia_state_publisher.launch.py']),
+            launch_arguments={'use_sim_time': use_sim_time}.items(),
+        ),
+
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([lidar_pkg_dir, LDS_LAUNCH_FILE]),
+            launch_arguments={'port': '/dev/ttyUSB0', 'frame_id': 'base_scan'}.items(), #port: /dev/ttyACM0は使わないから変更する！
+        ),
+
+        Node(
+            package='lucia_node',
+            executable='lucia_ros',
+            parameters=[lucia_param_dir],
+            #arguments=['-i', usb_port],
+            output='screen'),
+    ])
--- lucia_cartographer/launch/urg_node2_cartographer.launch.py
+++ lucia_cartographer/launch/urg_node2_cartographer.launch.py
@@ -27,9 +27,9 @@ from launch.substitutions import ThisLaunchFileDir
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='false')
-    turtlebot3_cartographer_prefix = get_package_share_directory('lucia_cartographer')
+    lucia_cartographer_prefix = get_package_share_directory('lucia_cartographer')
     cartographer_config_dir = LaunchConfiguration('cartographer_config_dir', default=os.path.join(
-                                                  turtlebot3_cartographer_prefix, 'config'))
+                                                  lucia_cartographer_prefix, 'config'))
     configuration_basename = LaunchConfiguration('configuration_basename',
                                                  default='urg_cartographer.lua')
 
